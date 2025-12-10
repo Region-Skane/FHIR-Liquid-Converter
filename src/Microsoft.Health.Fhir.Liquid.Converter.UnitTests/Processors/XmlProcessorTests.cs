71c4a117b8094441c8460e9e256c41a95bf55104
Peter Danielsson <242216@skane.se>
2025-12-10 08:22:24 +0000

    Merged PR 16694: Lagt till unittest + liquid för XmlArrayProcessor
    Lagt till unittest + liquid för XmlArrayProcessor
    
    ![bild (3).png](https://dev.azure.com/regionskane-utv/d02425f4-4791-4345-a3ae-26aed57c84ee/_apis/git/repositories/67ac8d87-1100-4898-ba4c-77be48e45038/pullRequests/16694/attachments/bild%20%283%29.png)
    
    Related work items: #95256

--- src/Microsoft.Health.Fhir.Liquid.Converter.UnitTests/Processors/XmlProcessorTests.cs
+++ src/Microsoft.Health.Fhir.Liquid.Converter.UnitTests/Processors/XmlProcessorTests.cs
@@ -1,9 +1,10 @@
-﻿// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using Microsoft.Health.Fhir.Liquid.Converter.Exceptions;
@@ -18,7 +19,6 @@ namespace Microsoft.Health.Fhir.Liquid.Converter.UnitTests.Processors
     {
         private static readonly string _xmlTestData;
         private static readonly string _jsonExpectData;
-        private static readonly XmlProcessor _xmlProcessor;
 
         static XmlProcessorTests()
         {
@@ -26,8 +26,6 @@ namespace Microsoft.Health.Fhir.Liquid.Converter.UnitTests.Processors
             {
                 _xmlTestData = File.ReadAllText(Path.Join(TestConstants.SampleDataDirectory, "Xml", "ExamplePatient.xml"));
                 _jsonExpectData = File.ReadAllText(Path.Join(TestConstants.ExpectedDirectory, "ExamplePatient.json"));
-                var processorSettings = new ProcessorSettings();
-                _xmlProcessor = new XmlLeafingProcessor(processorSettings, FhirConverterLogging.CreateLogger<XmlProcessor>());
             }
             catch (Exception ex)
             {
@@ -36,60 +34,104 @@ namespace Microsoft.Health.Fhir.Liquid.Converter.UnitTests.Processors
             }
         }
 
-        [Fact]
-        public void GivenXmlInput_WhenConvertWithXmlProcessor_CorrectResultShouldBeReturned()
-        {
-            var processor = _xmlProcessor;
-            var templateProvider = new TemplateProvider(TestConstants.XmlTemplateDirectory, DataType.XmlLeafing);
+        // Test cases: one for Array and one for Leafing
+        public static IEnumerable<object[]> ProcessorTestCases =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    "ExamplePatientXmlArray", // Liquid template for XmlArray
+                    DataType.XmlAlwaysArray,
+                    new Func<ProcessorSettings, XmlProcessor>(settings => new XmlArrayProcessor(settings, FhirConverterLogging.CreateLogger<XmlProcessor>())),
+                },
+                new object[]
+                {
+                    "ExamplePatient", // Liquid template for XmlLeafing
+                    DataType.XmlLeafing,
+                    new Func<ProcessorSettings, XmlProcessor>(settings => new XmlLeafingProcessor(settings, FhirConverterLogging.CreateLogger<XmlProcessor>())),
+                },
+            };
 
-            var result = processor.Convert(_xmlTestData, "ExamplePatient", templateProvider);
+        [Theory]
+        [MemberData(nameof(ProcessorTestCases))]
+        public void GivenXmlInput_WhenConvertWithXmlProcessor_CorrectResultShouldBeReturned(
+            string templateName,
+            DataType dataType,
+            Func<ProcessorSettings, XmlProcessor> processorFactory)
+        {
+            var processor = processorFactory(new ProcessorSettings());
+            var templateProvider = new TemplateProvider(TestConstants.XmlTemplateDirectory, dataType);
+
+            var result = processor.Convert(_xmlTestData, templateName, templateProvider);
+
             Assert.True(JToken.DeepEquals(JObject.Parse(_jsonExpectData), JToken.Parse(result)));
         }
 
-        [Fact]
-        public void GivenXmlInput_WhenConvertWithXmlProcessorInvalidTemplate_ThrowsException()
+        [Theory]
+        [MemberData(nameof(ProcessorTestCases))]
+        public void GivenXmlInput_WhenConvertWithXmlProcessorInvalidTemplate_ThrowsException(
+            string templateName,
+            DataType dataType,
+            Func<ProcessorSettings, XmlProcessor> processorFactory)
         {
-            var processor = _xmlProcessor;
-            var templateProvider = new TemplateProvider(TestConstants.XmlTemplateDirectory, DataType.XmlLeafing);
+            // Force use of templateName to avoid analyzer warning
+            _ = templateName;
+            var processor = processorFactory(new ProcessorSettings());
+            var templateProvider = new TemplateProvider(TestConstants.XmlTemplateDirectory, dataType);
 
-            // Attempt conversion with a non-existent template
-            var exception = Assert.Throws<RenderException>(() => processor.Convert(_xmlTestData, "NonExistentTemplate", templateProvider));
+            var exception = Assert.Throws<RenderException>(
+                () => processor.Convert(_xmlTestData, "NonExistentTemplate", templateProvider));
+
             Assert.Equal(FhirConverterErrorCode.TemplateNotFound, exception.FhirConverterErrorCode);
         }
 
-        [Fact]
-        public void GivenCancellationToken_WhenConvertWithXmlProcessor_CorrectResultsShouldBeReturned()
+        [Theory]
+        [MemberData(nameof(ProcessorTestCases))]
+        public void GivenCancellationToken_WhenConvertWithXmlProcessor_CorrectResultsShouldBeReturned(
+            string templateName,
+            DataType dataType,
+            Func<ProcessorSettings, XmlProcessor> processorFactory)
         {
-            var processor = _xmlProcessor;
-            var templateProvider = new TemplateProvider(TestConstants.XmlTemplateDirectory, DataType.XmlLeafing);
+            var processor = processorFactory(new ProcessorSettings());
+            var templateProvider = new TemplateProvider(TestConstants.XmlTemplateDirectory, dataType);
 
             var cts = new CancellationTokenSource();
-            var result = processor.Convert(_xmlTestData, "ExamplePatient", templateProvider, cts.Token);
+            var result = processor.Convert(_xmlTestData, templateName, templateProvider, cts.Token);
             Assert.True(result.Length > 0);
 
             // Cancel and attempt another conversion
             cts.Cancel();
-            Assert.Throws<OperationCanceledException>(() => processor.Convert(_xmlTestData, "ExamplePatient", templateProvider, cts.Token));
-            Console.WriteLine("TTT");
+            Assert.Throws<OperationCanceledException>(
+                () => processor.Convert(_xmlTestData, templateName, templateProvider, cts.Token));
         }
 
-        [Fact]
-        public void GivenProcessorSettings_WhenConvertWithXmlProcessor_CorrectResultsShouldBeReturned()
+        [Theory]
+        [MemberData(nameof(ProcessorTestCases))]
+        public void GivenProcessorSettings_WhenConvertWithXmlProcessor_CorrectResultsShouldBeReturned(
+            string templateName,
+            DataType dataType,
+            Func<ProcessorSettings, XmlProcessor> processorFactory)
         {
-            var defaultProcessor = new XmlLeafingProcessor(new ProcessorSettings(), FhirConverterLogging.CreateLogger<XmlProcessor>());
+            var defaultProcessor = processorFactory(new ProcessorSettings());
+
             var positiveTimeOutSettings = new ProcessorSettings { TimeOut = 1 };
-            var positiveTimeoutProcessor = new XmlLeafingProcessor(positiveTimeOutSettings, FhirConverterLogging.CreateLogger<XmlProcessor>());
+            var positiveTimeoutProcessor = processorFactory(positiveTimeOutSettings);
+
             var negativeTimeOutSettings = new ProcessorSettings { TimeOut = -1 };
-            var negativeTimeoutProcessor = new XmlLeafingProcessor(negativeTimeOutSettings, FhirConverterLogging.CreateLogger<XmlProcessor>());
+            var negativeTimeoutProcessor = processorFactory(negativeTimeOutSettings);
 
-            var templateProvider = new TemplateProvider(TestConstants.XmlTemplateDirectory, DataType.XmlLeafing);
+            var templateProvider = new TemplateProvider(TestConstants.XmlTemplateDirectory, dataType);
 
-            var result = defaultProcessor.Convert(_xmlTestData, "ExamplePatient", templateProvider);
+            // Default – should work
+            var result = defaultProcessor.Convert(_xmlTestData, templateName, templateProvider);
             Assert.True(result.Length > 0);
 
+            // Positive timeout – expecing timeout
             try
             {
-                var exception = Assert.Throws<RenderException>(() => positiveTimeoutProcessor.Convert(_xmlTestData, "TimeOutTemplate", templateProvider));
+                var exception = Assert.Throws<RenderException>(
+                    () => positiveTimeoutProcessor.Convert(_xmlTestData, "TimeOutTemplate", templateProvider));
+
                 Assert.Equal(FhirConverterErrorCode.TimeoutError, exception.FhirConverterErrorCode);
                 Assert.True(exception.InnerException is OperationCanceledException);
             }
@@ -98,7 +140,8 @@ namespace Microsoft.Health.Fhir.Liquid.Converter.UnitTests.Processors
                 Console.WriteLine("The operation did not time out as expected. Adjust template or logic to ensure timeout occurs.");
             }
 
-            result = negativeTimeoutProcessor.Convert(_xmlTestData, "ExamplePatient", templateProvider);
+            // Negativ timeout – expecting no timeout
+            result = negativeTimeoutProcessor.Convert(_xmlTestData, templateName, templateProvider);
             Assert.True(result.Length > 0);
         }
     }
